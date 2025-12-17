49a7939c25e84ff4aa8f679d7775755360356a18
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-12-17 10:32:41 +0100

    add custom hook useQuote.ts

---
+++ hooks/my/useQuote.ts
@@ -0,0 +1,25 @@
+import { getQuote } from "@/api/quotesApi";
+import { Quote } from "@/types/quote";
+import { useState } from "react";
+
+export default function useQuote() {
+  const [quote, setQuote] = useState<Quote | undefined>(undefined);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchQuote = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const data: Quote = await getQuote();
+      setQuote(data);
+    } catch (error) {
+      setError((error as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { quote, loading, error, fetchQuote };
+}
