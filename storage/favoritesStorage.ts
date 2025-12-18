ca8e74912cf6508ebb2f5f4d2c4bbc828db28ca8
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-12-18 10:11:46 +0100

    add first logic of favoritesStorage, so that the information persists

---
+++ storage/favoritesStorage.ts
@@ -0,0 +1,30 @@
+import { Quote } from "@/types/quote";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const FAVORITES_KEY = "favorites";
+
+export const saveFavorites = async (favorites: Quote[]): Promise<void> => {
+  try {
+    await AsyncStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites));
+  } catch (error) {
+    console.error("errore:", { error });
+  }
+};
+
+export const loadFavorites = async (): Promise<Quote[]> => {
+  try {
+    const data = await AsyncStorage.getItem(FAVORITES_KEY);
+    return data ? JSON.parse(data) : [];
+  } catch (error) {
+    console.error("errore nella lettura dallo storage:", { error });
+    return [];
+  }
+};
+
+export const removeFavorites = async (): Promise<void> => {
+  try {
+    await AsyncStorage.removeItem(FAVORITES_KEY);
+  } catch (error) {
+    console.error("errore nella rimozione totale dello storage:", { error });
+  }
+};
